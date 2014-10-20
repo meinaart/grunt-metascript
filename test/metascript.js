40feb2835033e3839dc046ee9c8db2efdccadf7a
Meinaart van Straalen <meinaart@kayenta.nl>
2014-10-20 23:01:10 +0200

    Hopefully fixed test by trimming output

--- test/metascript.js
+++ test/metascript.js
@@ -1,18 +1,20 @@
-
+/* global suite, test */
 var assert = require('assert');
 var fs = require('fs');
 var grunt = require('grunt');
 
 suite('grunt-metascript', function() {
+  'use strict';
+  
   suite('generating', function() {
     test('program mode', function() {
       assert.ok(fs.existsSync('out/somemeta-program.js'));
-      assert.equal(fs.readFileSync('out/somemeta-program.js', 'utf8'), fs.readFileSync('test/fixtures/somemeta-program.js', 'utf8'));
+      assert.equal(fs.readFileSync('out/somemeta-program.js', 'utf8').trim(), fs.readFileSync('test/fixtures/somemeta-program.js', 'utf8').trim());
     });
 
     test('transform mode', function() {
       assert.ok(fs.existsSync('out/somemeta-transform.js'));
-      assert.equal(fs.readFileSync('out/somemeta-transform.js', 'utf8'), fs.readFileSync('test/fixtures/somemeta-transform.js', 'utf8'));
+      assert.equal(fs.readFileSync('out/somemeta-transform.js', 'utf8').trim(), fs.readFileSync('test/fixtures/somemeta-transform.js', 'utf8').trim());
     });
 
     test('multiple files: exclude program', function() {
@@ -21,7 +23,8 @@ suite('grunt-metascript', function() {
 
     test('multiple files transformation', function() {
       grunt.file.recurse('out/multi/', function(abspath, rootdir, subdir, filename) {
-        assert.equal(fs.readFileSync(abspath, 'utf8'), fs.readFileSync('test/fixtures/multi/' + (subdir?subdir+'/':'') + filename, 'utf8'));
+        assert.ok(fs.existsSync(abspath));
+        assert.equal(fs.readFileSync(abspath, 'utf8').trim(), fs.readFileSync('test/fixtures/multi/' + (subdir?subdir+'/':'') + filename, 'utf8').trim());
       });
     });
   });
