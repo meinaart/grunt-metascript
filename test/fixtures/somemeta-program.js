c62dffb335055039f47a21b5fa441f394e734a61
Meinaart van Straalen <meinaart@kayenta.nl>
2014-10-20 23:23:18 +0200

    Fixed line endings

--- test/fixtures/somemeta-program.js
+++ test/fixtures/somemeta-program.js
@@ -1,121 +1,121 @@
 MetaScript(1,0,0);
 if (typeof WHAT === 'undefined') WHAT = false;
 if (typeof VERSION === 'undefined') VERSION = '1.0.0';
-  write('// Line expressions\r\n');
-  write('\r\n');
+  write('// Line expressions\n');
+  write('\n');
 if (WHAT) {
-  write('console.log("WHAT\'s true");\r\n');
+  write('console.log("WHAT\'s true");\n');
 } else {
-  write('console.log("WHAT\'s false");\r\n');
+  write('console.log("WHAT\'s false");\n');
 }
-  write('\r\n');
-  write('// Block expressions\r\n');
-  write('\r\n');
+  write('\n');
+  write('// Block expressions\n');
+  write('\n');
 if (WHAT)
-  write('console.log("WHAT\'s true");\r\n');
+  write('console.log("WHAT\'s true");\n');
 else
-  write('console.log("WHAT\'s false");\r\n');
-  write('\r\n');
+  write('console.log("WHAT\'s false");\n');
+  write('\n');
   write('console.log(');
 if (WHAT) {
   write('"WHAT\'s true"+');
 } else {
   write('"WHAT\'s false"+');
 }
-  write('"");\r\n');
-  write('\r\n');
-  write('// Snippets\r\n');
-  write('\r\n');
-  write('// before snippet\r\n');
+  write('"");\n');
+  write('\n');
+  write('// Snippets\n');
+  write('\n');
+  write('// before snippet\n');
 //...
 function bleh() {}
 //.
-  write('// after snippet\r\n');
-  write('\r\n');
-  write('// ?= expressions\r\n');
-  write('\r\n');
+  write('// after snippet\n');
+  write('\n');
+  write('// ?= expressions\n');
+  write('\n');
   write('MyLibrary.VERSION = ');
 write(JSON.stringify(VERSION));
-  write(';\r\n');
-  write('// or, alternatively, if VERSION is always string-safe:\r\n');
+  write(';\n');
+  write('// or, alternatively, if VERSION is always string-safe:\n');
   write('MyLibrary.VERSION = "');
 write(VERSION);
-  write('";\r\n');
-  write('// or, alternatively, if you don\'t mind a missing trailing semicolon:\r\n');
+  write('";\n');
+  write('// or, alternatively, if you don\'t mind a missing trailing semicolon:\n');
   write('MyLibrary.VERSION = ');
 write(JSON.stringify(VERSION));
 writeln();
-  write('// or, alternatively, if you like it procedural:\r\n');
+  write('// or, alternatively, if you like it procedural:\n');
   write('MyLibrary.VERSION = ');
 write(JSON.stringify(VERSION))
-  write(';\r\n');
-  write('// or, if you like it as a number even if you shouldn\'t:\r\n');
+  write(';\n');
+  write('// or, if you like it as a number even if you shouldn\'t:\n');
   write('MyLibrary.VERSION = ');
 write(JSON.stringify(parseFloat(VERSION)));
-  write(';\r\n');
-  write('\r\n');
-  write('// Utility functions using __\r\n');
-  write('\r\n');
+  write(';\n');
+  write('\n');
+  write('// Utility functions using __\n');
+  write('\n');
 myIndent = function(s) {
     write(indent(s+'\n', __));
 }
 __='    ';
 myIndent('hello("world");');
-  write('\r\n');
-  write('// Macros\r\n');
-  write('\r\n');
+  write('\n');
+  write('// Macros\n');
+  write('\n');
 __='';
 ASSERT_OFFSET = function(varname) {
   write('    if (');
 write(varname);
   write(' < 0 || ');
 write(varname);
-  write(' > this.capacity()) {\r\n');
+  write(' > this.capacity()) {\n');
   write('        throw(new RangeError("Illegal ');
 write(varname);
-  write('"));\r\n');
-  write('    }\r\n');
+  write('"));\n');
+  write('    }\n');
 }
-  write('function writeInt8(value, offset) {\r\n');
-  write('    // Here the macro is used:\r\n');
+  write('function writeInt8(value, offset) {\n');
+  write('    // Here the macro is used:\n');
 __='    ';
 ASSERT_OFFSET('offset');
-  write('    // ...\r\n');
-  write('}\r\n');
-  write('      \r\n');
-  write('// Indentation, includes and variable visibility\r\n');
-  write('\r\n');
-  write('// This will be indented (it\'s a ?= expression):\r\n');
+  write('    // ...\n');
+  write('}\n');
+  write('      \n');
+  write('// Indentation, includes and variable visibility\n');
+  write('\n');
+  write('// This will be indented (it\'s a ?= expression):\n');
   write('    ');
 write('var i=0;');
 writeln();
-  write('// Just like this (it uses manual indentation):\r\n');
+  write('// Just like this (it uses manual indentation):\n');
 write(indent('var j=0;\n', 4));
-  write('// Or this (it prepends __):\r\n');
+  write('// Or this (it prepends __):\n');
 write(__+'var k=0;\n');
-  write('// But this will not:\r\n');
+  write('// But this will not:\n');
 write('var k=0;\n');
-  write('// Got it?\r\n');
-  write('\r\n');
+  write('// Got it?\n');
+  write('\n');
 __='';
 YEP = true;
 var NOPE = false;
   write('// var NOPE = ');
 write(NOPE);
 writeln();
-  write('// This will be indented:\r\n');
+  write('// This will be indented:\n');
 __='    ';
 if (YEP) include("someinclude.js")
-  write('    // indented once\r\n');
-  write('// not indented\r\n');
-  write('\r\n');
-  write('// snip-snap\r\n');
-  write('\r\n');
+  write('    // indented once\n');
+  write('// not indented\n');
+  write('\n');
+  write('// snip-snap\n');
+  write('\n');
 __='';
 snip();
-  write('console.log("snippet");\r\n');
+  write('console.log("snippet");\n');
 var snippet = snap();
 write(snippet + snippet);
 writeln();
-  write('    \r\n');
-  write('// Before end\r\n');
+  write('    \n');
+  write('// Before end\n');
--- test/metascript.js
+++ test/metascript.js
@@ -1,20 +1,26 @@
 /* global suite, test */
+/* jshint quotmark: false */
 var assert = require('assert');
 var fs = require('fs');
 var grunt = require('grunt');
 
 suite('grunt-metascript', function() {
   'use strict';
+
+  function readFile(filename) {
+    var contents = fs.readFileSync(filename, 'utf8');
+    return contents.replace("\r", "");
+  }
   
   suite('generating', function() {
     test('program mode', function() {
       assert.ok(fs.existsSync('out/somemeta-program.js'));
-      assert.equal(fs.readFileSync('out/somemeta-program.js', 'utf8').trim(), fs.readFileSync('test/fixtures/somemeta-program.js', 'utf8').trim());
+      assert.equal(readFile('out/somemeta-program.js'), readFile('test/fixtures/somemeta-program.js'));
     });
 
     test('transform mode', function() {
       assert.ok(fs.existsSync('out/somemeta-transform.js'));
-      assert.equal(fs.readFileSync('out/somemeta-transform.js', 'utf8').trim(), fs.readFileSync('test/fixtures/somemeta-transform.js', 'utf8').trim());
+      assert.equal(readFile('out/somemeta-transform.js'), readFile('test/fixtures/somemeta-transform.js'));
     });
 
     test('multiple files: exclude program', function() {
@@ -24,7 +30,7 @@ suite('grunt-metascript', function() {
     test('multiple files transformation', function() {
       grunt.file.recurse('out/multi/', function(abspath, rootdir, subdir, filename) {
         assert.ok(fs.existsSync(abspath));
-        assert.equal(fs.readFileSync(abspath, 'utf8').trim(), fs.readFileSync('test/fixtures/multi/' + (subdir?subdir+'/':'') + filename, 'utf8').trim());
+        assert.equal(readFile(abspath), readFile('test/fixtures/multi/' + (subdir?subdir+'/':'') + filename));
       });
     });
   });
